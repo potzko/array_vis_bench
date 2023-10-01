dc8899066c3848f1e4e9203ca74d560a5f043381
potzko <orodnay@gmail.com>
2023-10-01 18:03:37 +0300

    small changes

--- src/sorts/heap_sort/weak_heap_sorts/weak_heap_sort.rs
+++ src/sorts/heap_sort/weak_heap_sorts/weak_heap_sort.rs
@@ -1,6 +1,6 @@
 const MAX_SIZE: usize = 100000;
 const BIG_O: &str = "O(N*log(N))";
-const NAME: &str = "heap sort";
+const NAME: &str = "weak heap sort";
 
 use crate::traits;
 
@@ -34,7 +34,7 @@ pub fn weak_heap_sort<T: Ord + Copy, U: traits::log_traits::SortLogger<T>>(
     let mut bottom_skips = vec![0; (n + 7) / 8];
     for i in (1..n).rev() {
         let mut ii = i;
-        while ii & 1 == get_flag(&bottom_skips, ii >> 1) {
+        while ii & 1 == get_bitwise_flag(&bottom_skips, ii >> 1) {
             ii >>= 1;
         }
         let gparent = ii >> 1;
@@ -44,10 +44,10 @@ pub fn weak_heap_sort<T: Ord + Copy, U: traits::log_traits::SortLogger<T>>(
     for i in (2..n).rev() {
         logger.swap(arr, 0, i);
         let mut current = 1;
-        let mut next = 2 * current + get_flag(&bottom_skips, current);
+        let mut next = 2 * current + get_bitwise_flag(&bottom_skips, current);
         while next < i {
             current = next;
-            next = 2 * current + get_flag(&bottom_skips, current);
+            next = 2 * current + get_bitwise_flag(&bottom_skips, current);
         }
         while current > 0 {
             weak_heap_merge(arr, &mut bottom_skips, 0, current, logger);
@@ -65,14 +65,14 @@ fn weak_heap_merge<T: Ord + Copy, U: traits::log_traits::SortLogger<T>>(
     logger: &mut U,
 ) {
     if logger.cond_swap_lt(arr, ind_a, ind_b) {
-        tog_flag(bottom_skips, ind_b);
+        toggle_bitwise_flag(bottom_skips, ind_b);
     }
 }
 
-fn get_flag(bottom_skips: &[usize], ind: usize) -> usize {
+fn get_bitwise_flag(bottom_skips: &[usize], ind: usize) -> usize {
     (bottom_skips[ind >> 3] >> (ind & 7)) & 1
 }
 
-fn tog_flag(bottom_skips: &mut [usize], ind: usize) {
+fn toggle_bitwise_flag(bottom_skips: &mut [usize], ind: usize) {
     bottom_skips[ind >> 3] ^= 1 << (ind & 7)
 }
